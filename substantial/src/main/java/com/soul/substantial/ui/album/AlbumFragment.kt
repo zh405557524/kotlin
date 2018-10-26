76b9e0381c43be078721d3a3e19c3042d4048604
zh405557524 <405557524@qq.com>
2018-10-26 15:52:17 +0800

    主界面搭建完成

---
+++ substantial/src/main/java/com/soul/substantial/ui/album/AlbumFragment.kt
@@ -0,0 +1,22 @@
+package com.soul.substantial.ui.album
+
+import android.view.View
+import com.soul.substantial.R
+import com.soul.substantial.base.BaseFragment
+
+
+class AlbumFragment : BaseFragment() {
+
+    override fun getRootView(): View {
+        return View.inflate(mContext, R.layout.fragment_album, null)
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+}
